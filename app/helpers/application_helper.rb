a84aa12e33a3fef5160a19dbb57be60d418b3bbc
miau715 <miau715@techbang.com.tw>
2011-12-19 18:34:12 +0800

    Added title functionally for show page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def title(*parts)
+    unless parts.empty?
+      content_for :title do
+        (parts << "Ticketee").join(" - ") unless parts.empty?
+      end
+    end
+  end
+  
 end
