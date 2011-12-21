1b465bee56576c34103d7e24fe7dd1a4de3df50d
miau715 <miau715@techbang.com.tw>
2011-12-21 16:46:57 +0800

    Added feature for signing in via the Devise-provided form

--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -1,5 +1,8 @@
 Given /^there are the following users:$/ do |table| 
   table.hashes.each do |attributes|
+    unconfirmed = attributes.delete("unconfirmed") == "true"
     @user = User.create!(attributes) 
+    @user.confirm! unless unconfirmed
   end
 end
+
