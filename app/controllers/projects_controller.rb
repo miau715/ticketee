c7026cef255a4b3da2ba4e2802a9b2e8304db348
miau715 <miau715@techbang.com.tw>
2011-12-22 12:25:43 +0800

    Restrict access to project actions to admins only

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,6 @@
 class ProjectsController < ApplicationController
   before_filter :find_project, :only => [:show, :edit, :update, :destroy]
+  before_filter :authorize_admin!, :except => [:index, :show]
   
   def index
     @projects = Project.all
