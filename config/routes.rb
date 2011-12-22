0c8b0a4cfd61f0cef7d59eb03684995fc7937dd6
miau715 <miau715@techbang.com.tw>
2011-12-22 15:43:12 +0800

    Added admin namespaced users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Ticketee::Application.routes.draw do
+
   devise_for :users
 
   # The priority is based upon order of creation:
@@ -50,6 +51,9 @@ Ticketee::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  namespace :admin do
+    resources :users
+  end
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
