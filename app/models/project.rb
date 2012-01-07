9a7b65c61fd994273a5509d06eda6b0b97fadc97
miau715 <miau715@techbang.com.tw>
2012-01-07 13:47:40 +0800

    Restricting projects API show to only users who have permission to view a project

--- app/models/project.rb
+++ app/models/project.rb
@@ -16,4 +16,11 @@ class Project < ActiveRecord::Base
     tickets.last
   end
   
+  def find_project 
+    @project = Project.for(current_user).find(params[:id])
+    rescue ActiveRecord::RecordNotFound 
+    flash[:alert] = "The project you were looking for could not be found." 
+    redirect_to projects_path
+  end
+  
 end
