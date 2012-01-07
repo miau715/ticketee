7324ba9b7b3bdee9b775c260cce57b989838ee78
miau715 <miau715@techbang.com.tw>
2012-01-07 15:52:43 +0800

    Added rate limiting to the V1 API

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,10 @@ class User < ActiveRecord::Base
   has_many :permissions
   before_save :ensure_authentication_token
   
+  def self.reset_request_count! 
+    update_all("request_count = 0", "request_count > 0")
+  end
+  
   def to_s
     "#{email} (#{admin? ? "Admin" : "User"})"
   end
