87e0f0d71886f3b6419c932431ef857c7018a920
miau715 <miau715@techbang.com.tw>
2012-01-07 14:47:47 +0800

    Projects can now be deleted through the API

--- app/controllers/api/v1/projects_controller.rb
+++ app/controllers/api/v1/projects_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::ProjectsController < Api::V1::BaseController 
   
   before_filter :authorize_admin!, :except => [:index, :show]
-  before_filter :find_project, :only => [:show, :update]
+  before_filter :find_project, :only => [:show, :update, :destroy]
   
   def index
     respond_with(Project.for(current_user))
@@ -24,6 +24,12 @@ class Api::V1::ProjectsController < Api::V1::BaseController
     @project.update_attributes(params[:project]) 
     respond_with(@project)
   end
+  
+  def destroy 
+    @project.destroy 
+    respond_with(@project)
+  end
+  
   
   private
     def find_project 
