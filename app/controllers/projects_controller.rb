35f11c702d86c391285901110aed1a2369e1ff36
miau715 <miau715@techbang.com.tw>
2012-01-31 13:41:09 +0800

    Implement tidier caching for the tickets list on the projects page

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,9 +2,9 @@ class ProjectsController < ApplicationController
   before_filter :authorize_admin!, :except => [:index, :show]
   before_filter :authenticate_user!, :only => [:index, :show]
   before_filter :find_project, :only => [:show, :edit, :update, :destroy]
-  caches_action :show, :cache_path => (proc do 
-    project_path(params[:id]) + "/#{current_user.id}/#{params[:page] || 1}"
-  end)
+#  caches_action :show, :cache_path => (proc do 
+#    project_path(params[:id]) + "/#{current_user.id}/#{params[:page] || 1}"
+#  end)
   
   def index
     @projects = Project.for(current_user).all
